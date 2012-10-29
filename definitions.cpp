0292a0bc925771744c38705843d77862a5f0485a
maja42 <maja.privat@gmx.at>
2012-10-29 12:04:26 +0100

    Update definitions.cpp

--- definitions.cpp
+++ definitions.cpp
@@ -1,79 +1,59 @@
 //Version 0.1
 //Jakob Maier
-
-#ifndef TYPEDEF_H_INCLUDED
-#define TYPEDEF_H_INCLUDED
-
-
-/** EINSTELLUNGEN **/
-    #define AutoAdjustWindowSize 1 //Ob bei jeder Loop, in der die Anzeige aktualisiert wird, auch die Fenstergröße angepasst werden soll
-
-    #define MaxXsize 20             //Maximale Spielfeldgröße in x-Richtung
-    #define MaxYsize 15             //Maximale Spielfeldgröße in y-Richtung
-    #define MaxSize POS{MaxXsize,MaxYsize}
+//...
+//Allgemeine Funktionen
 
 
-
-/** TYPE DEFINITIONS **/
-
-    struct POS{
-        int x,y;
-    };
-    struct AREA{
-        POS a;
-        POS b;
-    };
-    struct fPOS{
-        float x,y;
-    };
-    struct fAREA{
-        fPOS a;
-        fPOS b;
-    };
-
-
-    struct COLOR{
-        float r,g,b;
-    };
-    #define WHITE   COLOR{1.0f,1.0f,1.0f}
-    #define BLACK   COLOR{0.0f,0.0f,0.0f}
-    #define RED     COLOR{1.0f,0.0f,0.0f}
-    #define GREEN   COLOR{0.0f,1.0f,0.0f}
-    #define BLUE    COLOR{0.0f,0.0f,1.0f}
-    #define YELLOW  COLOR{1.0f,1.0f,0.0f}
-    #define MAGENTA COLOR{1.0f,0.0f,1.0f}
-    #define CYAN    COLOR{0.0f,1.0f,1.0f}
-
-
-    enum DIRECTION{
-        TOP,
-        BOTTOM,
-        LEFT,
-        RIGHT
-    };
-
-/** GLOBALE VARIABLEN **/
-
-    const POS windowSize={1200,680};      //Fenstergröße - Anzahl der Koordinatenpunkte die erreicht werden können
-    #define windX windowSize.x
-    #define windY windowSize.y
-
-    float coordPixel=1;                 //Wieviele Pixel eine Koordinateneinheit am Bildschirm bekommt
+const fAREA stdTextArea={{0.0f,0.0f},{1.0f,1.0f}};
 
 
 
 
-
-/** PROTOTYPEN **/
-
-    POS transformY(POS position);                                                                   //Die Y-Koordinate wird gespiegelt (Ursprung oben/unten vertauscht)
-    bool coordInside(POS coord,AREA gebiet);                                                        //Meldet, ob sich die übergebenen Koordinaten innerhalb des übergebenen Gebiets befinden (zB. für "Maus über Bild?)
-    void getMousePos(POS *coord);                                                                   //Gibt die Position der Maus im Koordinatensystem zurück
-    void PosSizeToArea(AREA *data);                                                                 //Wandelt eine Struktur, die aus Position+Größe besteht um, in Startposition+Endposition
-    AREA PosSizeToArea(AREA data);                                                                  //Wandelt eine Struktur, die aus Position+Größe besteht um, in Startposition+Endposition
-
+//Übergabeparameter:
+//  1.) position        [POS]       Koordinate, deren y-Wert gespiegelt werden soll
+//Rückgabewert:
+//  1.) position        [POS]       gespiegelte Koordinate
+void transformY(POS *position)//Die Y-Koordinate wird gespiegelt (Ursprung oben/unten vertauscht)
+{   position->y=windowSize.y-position->y;
+}
 
 
+//Übergabeparameter:
+//  1.) coord           [POS]       Koordinaten, die überprüft werden sollen
+//  2.) gebiet          [AREA]      Gebiet, das Überprüft werden soll
+//Rückgabewert:
+//  -)  Ob sich die Maus im Gebiet befindet (true: ja, false: nein)
+bool coordInside(POS coord,AREA gebiet)//Meldet, ob sich die übergebenen Koordinaten innerhalb des übergebenen Gebiets befinden (zB. für "Maus über Bild?)
+{   if((coord.x>=gebiet.a.x && coord.x<=gebiet.b.x) && (coord.y>=gebiet.a.y && coord.y<=gebiet.b.y))
+        return true;
+    return false;
+}
 
 
-#endif // TYPEDEF_H_INCLUDED
+//Rückgabeparameter:
+//  1.) coord           [POS]   Position der Maus im Koordinatensystem
+void getMousePos(POS *coord)//Gibt die Position der Maus im Koordinatensystem zurück
+{   glfwGetMousePos(&(coord->x),&(coord->y));
+    //Koordinatensystem umrechnen:
+    coord->x/=coordPixel;
+    coord->y/=coordPixel;
+    transformY(coord);
+}
+
+//Übergabeparameter:
+// 1.) *data            [AREA]
+//          data->a         Startposition
+//          data->b         Größe
+//Rückgabeparameter:
+// 1.) *data            [AREA]
+//          data->a         Startposition
+//          data->b         Endposition
+void PosSizeToArea(AREA *data)//Wandelt eine Struktur, die aus Position+Größe besteht um, in Startposition+Endposition
+{   data->b.x += data->a.x;
+    data->b.y += data->a.y;
+}
+AREA PosSizeToArea(AREA data)//Wandelt eine Struktur, die aus Position+Größe besteht um, in Startposition+Endposition
+{   data.b.x += data.a.x;
+    data.b.y += data.a.y;
+    return data;
+}
